8f4132304d34579a340d31315747c929b663a4df
Javs_Artez <oscar.artez@gmail.com>
2021-02-17 12:47:37 -0600

    Agregue css

---
+++ Proyecto_cajero/app.js
@@ -0,0 +1,21 @@
+let cuentas = [
+    { nombre: “Hiromi”, saldo: 200, password: ‘helloworld’ }
+    { nombre: “Manuel”, saldo: 290, password: ‘l33t’ }
+    { nombre: “Luis”, saldo: 67, password: ‘123’ }
+  ];
+
+//   const validar => (){
+   
+//     let usuario = document.getElementById("usuario").value;
+//     let contraseña = document.getElementById("contraseña").value;
+
+//     for (let i=0; i<cuentas.length;)
+//     if (usuario && contraseña === cuentas[0] && cuentas [2])
+  
+//   }
+
+
+// console.log(usuario,contraseña);
+
+
+//   
