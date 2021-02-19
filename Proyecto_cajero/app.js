6be92975a2bd7e7ee9a34a0e8bee623060c66992
oscar.artez <oscar.artez@gmail.com>
2021-02-18 18:39:40 -0600

    cambios en el java

--- Proyecto_cajero/app.js
+++ Proyecto_cajero/app.js
@@ -1,19 +1,24 @@
- const cuentas = [
+
+//DATOS//
+const cuentas = [
      { "nombre": 'Hiromi', "saldo": 200, "password": 'helloworld' },
      { "nombre": 'Manuel', "saldo": 290, "password": 'l33t' },
      { "nombre": 'Luis', "saldo": 67, "password": '123' }
    ];
 
 
-     function validacion(){
-       let usuario = document.getElementById("usuario").value;
-       let contraseña = document.getElementById("contraseña").value;
-
-       for ( let i=0; i < cuentas.length; i++){
-         let element = cuentas [i];
-         if (element.nombre === usuario && element.password === contraseña){
-
-          window.open("menu.html","");
-         }
-       }
+  function validacion(){
+    let usuario = document.getElementById("usuario").value;
+    let contraseña = document.getElementById("contraseña").value;
+    for ( let i=0; i < cuentas.length; i++){
+      let element = cuentas [i];
+      if (element.nombre === usuario && element.password === contraseña){
+       return true;
+    }  
    }
+  }
+      if (validacion () == true){
+         alert("Credenciales incorrectas");
+         }
+       else
+       window.location.href="menu.html";
